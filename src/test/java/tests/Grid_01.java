72b3bb866f89e4909b1a0542c94b483d1943c7be
lutfi <lutfikavalci@gmail.com>
2023-07-05 13:25:08 +0300

    first commit

---
+++ src/test/java/tests/Grid_01.java
@@ -0,0 +1,41 @@
+package tests;
+
+import manager.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Grid_01 {
+
+    DriverManager driverManager = new DriverManager();
+    static WebDriver driver;
+    public static void main(String[] args) throws MalformedURLException {
+
+        driver = new RemoteWebDriver(new URL("http://192"), new ChromeOptions());
+
+        driver.get("https://www.google.com");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+    }
+
+    @Test
+    void remoteChromeDriverTest(){
+
+        driver = driverManager.setUpChromeDriver();
+        driver.get("https://www.amazon.com");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+    }
+
+    @Test
+    void remoteEdgeDriverTest(){
+        driver=driverManager.setUpEdgeDriver();
+        driver.get("www.youtube.com");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+    }
+}
---
+++ src/test/java/tests/Grid_02.java
@@ -0,0 +1,27 @@
+package tests;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class Grid_02 {
+
+    @Parameters("browser")
+    @BeforeTest
+    void beforeTest()
+
+
+    @Test
+    void  test01(){
+
+    }
+}
+/*
+cho "# SeleniumGrid_Csp" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:Lkavalci/SeleniumGrid_Csp.git
+git push -u origin main
+ */
